030dd9a833a89b16ff90c59a232677bb97dc2060
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2021-05-23 22:59:29 +0300

    right connect styles, scripts and deregister Jquery

--- wp-content/themes/maket-theme/functions.php
+++ wp-content/themes/maket-theme/functions.php
@@ -2,13 +2,33 @@
 // --- theme style ---
 add_action( 'wp_enqueue_scripts', 'maket_styles' );
 function maket_styles() {
-	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	// <!-- Основной файл стилей темы -->
+	wp_enqueue_style( 'maket-style', get_stylesheet_uri() );
+	// <!-- Слайдер -->
+	wp_enqueue_style( 'slick', get_template_directory_uri() . '/slick/slick.css');
+	wp_enqueue_style( 'slick-theme', get_template_directory_uri() . '/slick/slick-theme.css');
+	// <!-- Анимация -->
+	wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.css');
+	// <!-- Bootstrap -->
+	wp_enqueue_style( 'bootstrap-grid', get_template_directory_uri() . '/css/bootstrap-grid.min.css');
+	// <!-- Custom styles for this template -->
+	wp_enqueue_style( 'maket-custom-style', get_template_directory_uri() . '/css/style.css');
+}
 
-	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+add_action( 'wp_enqueue_scripts', 'jquery' );
+function jquery() {
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
+	wp_enqueue_script( 'jquery' );
 }
 
 // --- theme scripts ---
 add_action( 'wp_enqueue_scripts', 'maket_scripts' );
 function maket_scripts() {
-  	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+  	wp_enqueue_script( 'slick', get_template_directory_uri() . '/slick/slick.min.js', 'jquery', null, true );
+  	wp_enqueue_script( 'maskedinput', get_template_directory_uri() . '/js/jquery.maskedinput.min.js', 'jquery', null, true );
+  	wp_enqueue_script( 'wow', get_template_directory_uri() . '/js/wow.min.js', null, null, true );
+  	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', null, null, true );
+
+
 }
